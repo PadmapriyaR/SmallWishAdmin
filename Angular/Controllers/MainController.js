4b8bc0bd7613fe8cd76fc265050a399ced665fe5
Padmapriya <padmapriya2431995@gmail.com>
2018-03-03 00:36:02 +0545

    Small Wish Admin

---
+++ Angular/Controllers/MainController.js
@@ -0,0 +1,154 @@
+﻿
+//main controller
+main.controller("MainController", function ($scope, $http, $localStorage) {
+    /*$scope.ABC = "ABC";
+    $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];*/
+  $scope.speakerimage = [{'speaker': 'img1'}];
+  $scope.choice = {};
+  $scope.names = [{'id': 'choice1'}];
+  $scope.aboutspeakers = [{'name': 'speaker1'}];
+  	$scope.addNewChoice = function() {
+  		var newItemNo = $scope.names.length+1;
+  		var newdesc = $scope.names.length+1;
+  		var newImg = $scope.names.length+1;
+  		$scope.names.push({'id':'choice'+newItemNo});
+  		$scope.aboutspeakers.push({'name':'speaker'+newdesc});
+  		$scope.speakerimage.push({'speaker': 'img'+newImg});
+  		};
+	$scope.showAddChoice = function(choice, id) {
+  return choice.id === $scope.names[$scope.names.length-1].id;
+
+};
+
+	//Agenda
+	  
+  	  $scope.timings = [{'id': 'time1'}];
+  	  $scope.aboutagenda = [{'agenda': 'topic1'}];
+  	$scope.addNewAgenda = function() {
+  		var newNo = $scope.timings.length+1;
+  		var newagenda = $scope.names.length+1;
+  		$scope.timings.push({'id':'time'+newNo});
+  		$scope.aboutagenda.push({'agenda':'topic'+newagenda});
+  		};
+	
+	//Add menus
+	$scope.menus = [{'menu': 'menu1'}];
+	$scope.addNewMenu = function(){
+		var newMenu = $scope.menus.length+1;
+		$scope.menus.push({'menu':'menu'+newMenu});
+	}
+
+	//Menu
+	$scope.saveMenus = function(){
+		$localStorage.menus = $scope.menuDetails;
+		console.log('localStorage', $localStorage.menus); 
+		$scope.menuItem = $localStorage.menus;
+	};
+
+
+  //create event
+	$scope.createEvent = function(){
+		console.log('Post Wish');
+		console.log($scope.admin);
+		$http.post('/createevents', $scope.admin).success(function(response){
+			console.log(response);
+			$scope.admin = '';
+			$scope.successTextAlert = "Wish has been posted";
+			$scope.showSuccessAlert = true;
+			$localStorage.save = response;
+			console.log('localStorage',$localStorage.save)
+			// switch flag
+			$scope.switchBool = function(value) {
+   			$scope[value] = !$scope[value];
+		};
+			
+		});
+	};
+
+	//Send Thanks
+	$scope.buttonStyle = "white";
+	$scope.sendThanks = function(id){
+		console.log('sending thanks notes');
+		console.log(id);
+		$scope.buttonStyle = "teal";		
+	};
+
+	var refresh = function(){
+		$http.get('/allevents').success(function(response){
+			$scope.admins = response;
+			$scope.admin = '';
+		});
+	}	
+	//show Events
+	$scope.showEvents = function(){
+		$http.get('/allevents').success(function(response){
+			$scope.admins = response;
+			$scope.display = $scope.admins;
+			$scope.admin = '';
+
+		});
+	};
+
+	//Edit event status
+	$scope.editEvent = function(id){
+		console.log('I am from edit function');
+		console.log(id);
+		$http.get('/allevents/'+id).success(function(response){
+			$scope.values = response;
+			$scope.admin = response;
+		});
+	};
+
+	//update Event
+	$scope.updateStatus = function(){
+		console.log('id '+$scope.admin._id);
+		console.log('Status'+ $scope.admin.status);
+		var sid = $scope.admin._id;
+		var sname = $scope.admin.status;
+		var sendThanks = $scope.thanks;
+		$http.put('/createevents/'+$scope.admin._id, $scope.admin, $scope.thanks).success(function(response){
+			console.log(response);
+			$scope.admins = '';
+			//$scope.showEvents();
+			refresh();
+		});
+	};
+	//Delete Event
+	$scope.deleteEvent = function(){
+		var arraylength;
+		$http.get('/allevents').success(function(response){
+			console.log(response);
+			$scope.flag = response;
+			arraylength = $scope.flag.length;
+			console.log('arraylength:' + arraylength);
+			if(arraylength == 0){
+			console.log('Cant delete');
+			$scope.successAlert = "No wishes Exist.";
+			$scope.showAlert = true;
+			$scope.switchBooll = function(value) {
+   			$scope[value] = !$scope[value];
+		};
+		}
+		else{
+			$http.delete('/deleteevent').success(function(response){
+			console.log(response);
+			$scope.successAlert = "Previous wishes has been deleted Successfully";
+			$scope.showAlert = true;
+
+			// switch flag
+			$scope.switchBooll = function(value) {
+   			$scope[value] = !$scope[value];
+		};
+		});
+
+		}
+		});
+	};	
+});
---
+++ server.js
@@ -0,0 +1,120 @@
+//adding opensource modules to application 
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt-nodejs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var mongoose = require('mongoose');
+var models_user = require('./Angular/Models/user.js');
+var mongojs = require("mongojs");
+var db = mongojs('mongodb://websitedb:websitedb@ds025399.mlab.com:25399/websitedb', ['websitedb']);
+
+
+//connection database
+mongoose.connect('mongodb://websitedb:websitedb@ds025399.mlab.com:25399/websitedb');
+
+//import the routers
+var router = require('./Routes/router');
+var authenticate = require('./Routes/authentication')(passport);
+
+//for using express throughout this application
+var app = express();
+/*app.post('/createevents', function(req,res){
+	console.log('I am from api');
+	db.AngularizeApp.insert(req.body, function(err, docs){
+		res.json(docs);
+	});
+});*/
+//tell node that My application will use ejs engine for rendering, view engine setup
+app.set('views', path.join(__dirname, 'Views'));
+app.set('view engine', 'ejs');
+
+//tell node the global configuration about parser,logger and passport
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(session({
+  secret: 'keyboard cat'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize()); //initializing passport
+app.use(passport.session()); //initializing passport session
+
+//tell node about these directories that application may get resources from
+app.use('/', router);
+app.use('/auth', authenticate);
+app.use(express.static(path.join(__dirname, 'scripts')));
+app.use(express.static(path.join(__dirname, 'Content')));
+app.use(express.static(path.join(__dirname, 'Angular')));
+app.use(express.static(path.join(__dirname, 'Views/Main')));
+app.use(express.static(path.join(__dirname, 'public/images')));
+app.use(express.static(path.join(__dirname, 'Views/Authentication')));
+
+
+//providing auth-api to passport so that it can use it.
+var initPassport = require('./Passport/passport-init');
+initPassport(passport);
+
+app.post('/createevents', function(req, res){
+	console.log(req.body);
+	db.websitedb.insert(req.body, function(err, docs){
+		console.log('inserted');
+		res.json(docs);
+	});
+});
+app.get('/allevents', function(req, res){
+		console.log('I got a request from server');
+		db.websitedb.find(function(err, docs) {
+			console.log(docs);
+			res.json(docs);
+		});
+});
+app.get('/allevents/:id', function(req, res){
+	var id = req.params.id;
+	console.log(id);
+	db.websitedb.findOne({ _id: mongojs.ObjectId(id)}, function(err, docs){
+		if(err)
+			res.send(err);
+		else
+			console.log(docs);
+			res.json(docs);
+	});
+});
+app.put('/createevents/:id', function(req, res){
+	var id = req.params.id;
+	console.log('update status' +id);
+	console.log('params' + req.body.staus);
+	console.log('PUT' +req.body);
+	console.log('mongojs id' + mongojs.ObjectId(id));
+	db.websitedb.update({_id: mongojs.ObjectId(id)}, {$set: {status: req.body.status}}, function(err, docs){
+		if(err)
+			res.send(err);
+		else
+			console.log(docs);
+			res.json(docs);
+	});
+});
+app.delete('/deleteevent', function(req, res){
+	db.websitedb.remove({}, function(err, docs){
+		console.log('Deleted');
+		res.json(docs);
+	});
+});
+
+//running server on node
+app.listen(process.env.PORT || 3000);
+console.log('Magic is happening on port number 3000!!');
+
+/*var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});*/
+
+//exporting this application as a module
+module.exports = app;
